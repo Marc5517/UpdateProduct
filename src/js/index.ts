909be6aad70d8f0d66f37a80d280c55941c3fdbc
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2021-09-20 12:58:05 +0200

    Lavet en side til at opdatere en vare

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,47 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface IProduct {
+    productId: number
+    productNr: number
+    customerNr: number
+    invoiceNr: number
+    serialNr: string
+}
+
+let baseUrl: string = "https://customerrestservice.azurewebsites.net/api/Products"
+
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        updateData: { productId: "", productNr: "", customerNr: "", invoiceNr: "", serialNr: ""},
+        updateMessage: "",
+    },
+    created() {
+        let params = new URLSearchParams(location.search);
+        this.updateData.productId = params.get('productId')
+        this.updateData.productNr = params.get('productNr')
+        this.updateData.customerNr = params.get('customerNr')
+        this.updateData.invoiceNr = params.get('invoiceNr')
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        updateProduct() {
+            let url: string = baseUrl + "/" + this.updateData.productId
+            axios.put<IProduct>(url, this.updateData)
+                .then((response: AxiosResponse) => {
+                    let message: string = response.statusText + " varen er opdateret."
+                    this.updateMessage = message
+                })
+                .catch((error: AxiosError) => {
+                    // this.addMessage = error.message
+                    alert(error.message)
+                })
         }
     }
 })
